d0c178fb08a9b4ec3a30416d99dd21ebe5621ca0
DcViet <tranducviet.eng@gmail.com>
2023-10-24 23:19:50 +0700

    update

---
+++ vendor/data.js
@@ -0,0 +1,23 @@
+const DATA = [{
+        id: 1,
+        title: 'Coffees &amp; Teas',
+        slogan: 'Blended to Perfection',
+        imagepath: 'img/products-01.jpg'
+    }, 
+    {
+        id: 2,
+        title: 'Bakery &amp; Kitchen',
+        slogan: 'Delicious Treats, Good Eats',
+        imagepath: 'img/products-02.jpg'
+    }, 
+    {
+        id: 3,
+        title: 'From Around the World',
+        slogan: 'Bulk Speciality Blends',
+        imagepath: 'img/products-03.jpg'
+    }
+];
+
+function getData() {
+    return DATA;
+}
